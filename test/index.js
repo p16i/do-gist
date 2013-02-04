5672f1c8ce7a845dd6c6250d78ac9dcce7b49adb
Pattarawat Chormai <goonnow@rocketmail.com>
2013-02-04 22:32:59 +0700

    Use  module to get a clipboard in different platforms

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 var myGist = require("../");
 var assert = require("assert");
-var execSync = require("exec-sync");
+var clipboard = require("copy-paste");
 
 it('should return -1 when the value is not present', function(){
 
     /* Mock Clipboard */
     var str = "TEST GIST";
-    execSync("echo "+str+" | pbcopy");
+    copy(str);
 
     assert.equal( myGist.doGist(), str );
 });
