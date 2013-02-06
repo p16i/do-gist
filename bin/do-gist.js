ae9d0e45afcd85a1aec616a865d34793ad2901bc
Pattarawat Chormai <goonnow@rocketmail.com>
2013-02-06 22:39:05 +0700

    DG-1: Implement anonymous feature.

--- bin/do-gist.js
+++ bin/do-gist.js
@@ -1,4 +1,17 @@
-#!/usr/local/bin/node
+#!/usr/bin/env node
 var myGist = require("../");
+var argv = require('optimist').argv;
 
-myGist.doGist();
+if( argv.l ){
+    myGist.doGist({isAnonymous: 0});
+}else if( argv.a ) {
+    myGist.doGist({isAnonymous: 1});
+}else{
+    printUsage();
+}
+
+function printUsage(){
+    console.log("Usage : do-gist [-l,-a]");
+    console.log(" -a : create anonymous gist");
+    console.log(" -l : create your own gist");
+}
